6edf87891c1d6d93dbcb2d86f3f6f82327c69694
Arvin Matthew Rapstad <arvinrapstad@gmail.com>
2024-10-21 14:26:29 +0200

    Fix minor code error

--- HexGame/createData.py
+++ HexGame/createData.py
@@ -5,11 +5,11 @@ def createCSV(board_size, examples, goBack, CSVname):
     with open(CSVname+".csv", 'w', newline='') as file:
         writer = csv.writer(file)
         field = ["winner", "feature", "edges"]
+        writer.writerow(field)
 
         for i in range(examples):
             new_game = game.Game(board_size)
             winner, feature, edges = new_game.SimulateGame(goBack)
-            writer.writerow(field)
             writer.writerow([winner, feature, edges])
 
 
