8779962482292e82aab30d09eecc0050a1c8ec63
Arvin Rapstad <101880810+Athiene@users.noreply.github.com>
2024-12-04 17:07:41 +0100

    Minor change for printing during data generation

--- Helper/data_generation.py
+++ Helper/data_generation.py
@@ -153,10 +153,7 @@ def fetch_simulation_games(number, gameboard_size, goBack, randomMoves, maxMoves
         elif winner == "1" and len(blue_data) < number:
             blue_data.append((winner, feature))
             counting += 1
-            
-        
-        print(f"Genered {counting}/{(number*2)} games")
-            
+        print(f"Genered {counting}/{number*2} games")
     return red_data + blue_data
 
 def createData(gameboard_size, csvName, number_of_examples, go_back, random_moves, maxMoves):
