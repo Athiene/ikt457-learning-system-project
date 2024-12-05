ed1d6d6d6a2974c55904e0a41fe74aa4661a6f22
Arvin Rapstad <101880810+Athiene@users.noreply.github.com>
2024-12-05 17:22:52 +0100

    Modified the CSVname to include if it is random or strategy based moves

--- Simulations/13x13/createData.py
+++ Simulations/13x13/createData.py
@@ -11,8 +11,10 @@ Gameboard_size = 13
 # This will be 2x, so if you write 1000, it will be 2000 number of examples
 Number_of_examples = 10000
 Go_back=0
-Random_moves = True
-CSV_name = f"{Gameboard_size}x{Gameboard_size}_goBack{Go_back}_set"
+Random_moves = False
+CSV_name = f"SM_{Gameboard_size}x{Gameboard_size}_goBack{Go_back}_set"
+if Random_moves:
+    CSV_name = f"RM_{Gameboard_size}x{Gameboard_size}_goBack{Go_back}_set"
 data_generation.createData(gameboard_size=Gameboard_size, csvName=CSV_name, number_of_examples=Number_of_examples, go_back=Go_back, random_moves=Random_moves, maxMoves=12)
 
     
